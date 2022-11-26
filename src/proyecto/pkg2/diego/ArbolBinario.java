dfde70419c1ddc8ccb8843fd0bd803ff89a66c91
Justdg74 <ddigiosia@correo.unimet.edu.ve>
2022-11-26 10:04:51 -0400

    Conversiones listas

--- src/proyecto/pkg2/diego/ArbolBinario.java
+++ src/proyecto/pkg2/diego/ArbolBinario.java
@@ -163,4 +163,6 @@ public class ArbolBinario <T>{
         
     }
     
+    
+    
 }
--- src/proyecto/pkg2/diego/Conversion.java
+++ src/proyecto/pkg2/diego/Conversion.java
@@ -98,151 +98,166 @@ public class Conversion {
 
     }
     
-    public boolean isOperator(char c) {
-        return (!(c >= 'a' && c <= 'z')
-                && !(c >= '0' && c <= '9')
-                && !(c >= 'A' && c <= 'Z'));
-    }
-
-// Function to find priority
-// of given operator.
-    public int getPriority(char C) {
-        if (C == '-' || C == '+') {
-            return 1;
-        } else if (C == '*' || C == '/') {
-            return 2;
-        } else if (C == '^') {
-            return 3;
+//    public boolean isOperator(char c) {
+//        return (!(c >= 'a' && c <= 'z')
+//                && !(c >= '0' && c <= '9')
+//                && !(c >= 'A' && c <= 'Z'));
+//    }
+//
+//// Function to find priority
+//// of given operator.
+//    public int getPriority(char C) {
+//        if (C == '-' || C == '+') {
+//            return 1;
+//        } else if (C == '*' || C == '/') {
+//            return 2;
+//        } else if (C == '^') {
+//            return 3;
+//        }
+//        return 0;
+//    }
+//
+//// Function that converts infix
+//// expression to prefix expression.
+//    public String infixToPrefix(String infix) {
+//        // stack for operators.
+//        Pila operators = new Pila();
+//
+//        // stack for operands.
+//        Pila operands = new Pila();
+//
+//        for (int i = 0; i < infix.length(); i++) {
+//
+//            // If current character is an
+//            // opening bracket, then
+//            // push into the operators stack.
+//            if (infix.charAt(i) == '(') {
+//                operators.push(infix.charAt(i));
+//            } // If current character is a
+//            // closing bracket, then pop from
+//            // both stacks and push result
+//            // in operands stack until
+//            // matching opening bracket is
+//            // not found.
+//            else if (infix.charAt(i) == ')') {
+//                while (!operators.IsEmpty()
+//                        && (char)operators.peek() != '(') {
+//
+//                    // operand 1
+//                    String op1 = String.valueOf(operands.peek());
+//                    operands.pop();
+//
+//                    // operand 2
+//                    String op2 = String.valueOf(operands.peek());
+//                    operands.pop();
+//
+//                    // operator
+//                    char op = (char)operators.peek();
+//                    operators.pop();
+//
+//                    // Add operands and operator
+//                    // in form operator +
+//                    // operand1 + operand2.
+//                    String tmp = op + op2 + op1;
+//                    operands.push(tmp);
+//                }
+//
+//                // Pop opening bracket
+//                // from stack.
+//                operators.pop();
+//            } // If current character is an
+//            // operand then push it into
+//            // operands stack.
+//            else if (!isOperator(infix.charAt(i))) {
+//                operands.push(infix.charAt(i) + "");
+//            } // If current character is an
+//            // operator, then push it into
+//            // operators stack after popping
+//            // high priority operators from
+//            // operators stack and pushing
+//            // result in operands stack.
+//            else {
+//                while (!operators.IsEmpty()
+//                        && getPriority(infix.charAt(i))
+//                        <= getPriority((char)operators.peek())) {
+//
+//                    String op1 = String.valueOf(operands.peek());
+//                    operands.pop();
+//
+//                    String op2 = String.valueOf(operands.peek());
+//                    operands.pop();
+//
+//                    char op = (char)operators.peek();
+//                    operators.pop();
+//
+//                    String tmp = op + op2 + op1;
+//                    operands.push(tmp);
+//                }
+//
+//                operators.push(infix.charAt(i));
+//            }
+//        }
+//
+//        // Pop operators from operators
+//        // stack until it is empty and
+//        // operation in add result of
+//        // each pop operands stack.
+//        while (!operators.IsEmpty()) {
+//            String op1 = String.valueOf(operands.peek());
+//            operands.pop();
+//
+//            String op2 = String.valueOf(operands.peek());
+//            operands.pop();
+//
+//            char op = (char)operators.peek();
+//            operators.pop();
+//
+//            String tmp = op + op2 + op1;
+//            operands.push(tmp);
+//        }
+//
+//        // Final prefix expression is
+//        // present in operands stack.
+//        return String.valueOf(operands.peek());
+//    }
+    
+    public boolean IsOperator(char x) {
+        switch (x) {
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+            case '^':
+            case '%':
+                return true;
         }
-        return 0;
+        return false;
     }
 
-// Function that converts infix
-// expression to prefix expression.
-    public String infixToPrefix(String infix) {
-        // stack for operators.
-        Pila operators = new Pila();
+    public String convert(String str) {
+        Pila stack = new Pila();
 
-        // stack for operands.
-        Pila operands = new Pila();
+        // Length of expression
+        int l = str.length();
 
-        for (int i = 0; i < infix.length(); i++) {
+        // Reading from right to left
+        for (int i = l - 1; i >= 0; i--) {
+            char c = str.charAt(i);
+            if (IsOperator(c)) {
+                String op1 = String.valueOf(stack.pop());
+                String op2 = String.valueOf(stack.pop());
 
-            // If current character is an
-            // opening bracket, then
-            // push into the operators stack.
-            if (infix.charAt(i) == '(') {
-                operators.push(infix.charAt(i));
-            } // If current character is a
-            // closing bracket, then pop from
-            // both stacks and push result
-            // in operands stack until
-            // matching opening bracket is
-            // not found.
-            else if (infix.charAt(i) == ')') {
-                while (!operators.IsEmpty()
-                        && (char)operators.peek() != '(') {
+                // Concat the operands and operator
+                String temp = "(" + op1 + c + op2 + ")";
+                stack.push(temp);
+            } else {
 
-                    // operand 1
-                    String op1 = String.valueOf(operands.peek());
-                    operands.pop();
-
-                    // operand 2
-                    String op2 = String.valueOf(operands.peek());
-                    operands.pop();
-
-                    // operator
-                    char op = (char)operators.peek();
-                    operators.pop();
-
-                    // Add operands and operator
-                    // in form operator +
-                    // operand1 + operand2.
-                    String tmp = op + op2 + op1;
-                    operands.push(tmp);
-                }
-
-                // Pop opening bracket
-                // from stack.
-                operators.pop();
-            } // If current character is an
-            // operand then push it into
-            // operands stack.
-            else if (!isOperator(infix.charAt(i))) {
-                operands.push(infix.charAt(i) + "");
-            } // If current character is an
-            // operator, then push it into
-            // operators stack after popping
-            // high priority operators from
-            // operators stack and pushing
-            // result in operands stack.
-            else {
-                while (!operators.IsEmpty()
-                        && getPriority(infix.charAt(i))
-                        <= getPriority((char)operators.peek())) {
-
-                    String op1 = String.valueOf(operands.peek());
-                    operands.pop();
-
-                    String op2 = String.valueOf(operands.peek());
-                    operands.pop();
-
-                    char op = (char)operators.peek();
-                    operators.pop();
-
-                    String tmp = op + op2 + op1;
-                    operands.push(tmp);
-                }
-
-                operators.push(infix.charAt(i));
+                // To make character to string
+                stack.push(c + "");
             }
         }
-
-        // Pop operators from operators
-        // stack until it is empty and
-        // operation in add result of
-        // each pop operands stack.
-        while (!operators.IsEmpty()) {
-            String op1 = String.valueOf(operands.peek());
-            operands.pop();
-
-            String op2 = String.valueOf(operands.peek());
-            operands.pop();
-
-            char op = (char)operators.peek();
-            operators.pop();
-
-            String tmp = op + op2 + op1;
-            operands.push(tmp);
-        }
-
-        // Final prefix expression is
-        // present in operands stack.
-        return String.valueOf(operands.peek());
+        return String.valueOf(stack.pop());
     }
     
-    
-//    public String convertInfixToPrefix(String expr){
-//        String result = "";
-//        String aux = "";
-//        
-//        for (int i = expr.length() - 1; i >= 0; i--) {
-//            aux += expr.charAt(i);
-//        }
-//        
-//        
-//        String aux2 = aux.replace('(', ',').replace(')', '.');
-//        String aux3 = aux2.replace(',', ')').replace('.', '(');
-//        
-//        System.out.println(aux3);
-//        String temp = covertInfixToPostfix(aux3);
-//
-//        for (int i = temp.length() - 1; i >= 0; i--) {
-//            result += temp.charAt(i);
-//        }
-//        
-//        return result;
-//        
-//    }
      
 }
